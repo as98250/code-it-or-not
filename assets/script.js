ccb8c1e406059930d032d86d04e50e0aae41fe10
Ariell SCott <scott.ariell00@gmail.com>
2021-08-26 18:18:33 -0400

    changing questino to an array

--- assets/script.js
+++ assets/script.js
@@ -1,148 +1,133 @@
 let start = document.getElementById('start-btn')
-let cardElement = document.getElementById('main-card-container')
+let cardElement = document.getElementById('mainCard')
 let questionContainer = document.getElementById('question')
 let answers = document.getElementById('answer-container')
 
+let timeEl = document.getElementById('iTimeShow')
+var currentIndex = 0;
+var myQuestions = [
+    {
+        question: "What is the correct algorithm for a for-loop?",
+        answers: [
+            'for ( var i = 0; i > array.length; i++)',
+            'for ( var i = 0; i < array.length; i--)',
+            'for ( var i = 0; i < array.length; i++)',
+            'for ( var i = 0, i > array.length; i--)'
+        ],
+        correctAnswer: 2
+    },
+    {
+        question: "Which of the following is NOT used for DECLARATIONS?",
+        answers: [
+            'var',
+            'use',
+            'const',
+            'let'
+        ],
+        correctAnswer: 1
+    },
+    {
+        question: "JavaScript is a [blank] typed and [blank] language.",
+        answers: [
+            'loosely, dynamic',
+            'strongly, dynamic',
+            'loosely, static',
+            'strongly, static'
+        ],
+        correctAnswer: 0
+    },
+    {
+        question: "[blank] represents a logical entity and can have a value of either true or false.",
+        answers: [
+            'array',
+            'string',
+            'null',
+            'boolean'
+        ],
+        correctAnswer: 3
+    },
+    {
+        question: "A variable declared in [blank] is available to all functions?",
+        answers: [
+            'local scope',
+            'internal scope',
+            'global scope',
+            'wide scope'
+        ],
+        correctAnswer: 2
+    },
+    {
+        question: "[blank] are reusable blacks of code that perform a specific task?",
+        answers: ['objects',
+            'declarations',
+            'perameters',
+            'functions'
+        ],
+        correctAnswer: 3
+    },
+    {
+        question: "Which of the following stops the functions from being executed?",
+        answers: [
+
+            'return',
+            'stop',
+            'pause',
+            'loop'
+        ],
+        correctAnswer: 0
+    },
+    {
+        question: "which represents equal value and equal type?",
+        answers: [
+            '==',
+            '===',
+            '=',
+            '/='
+        ],
+        correctAnswer: 1
+    },
+    {
+        question: "[Blank] are a collection of [blank]. [Blank] are made up of key-value pairs?",
+        answers: [
+            'objects, properties, properties',
+            'properties, objects, objects',
+            'objects, properties, objects',
+            'properties, objects, properties'
+        ],
+        correctAnswer: 0
+    },
+    {
+        question: "True or False: Functions can not be called again to execute the block of code.",
+        answers: [
+            'true',
+            'false'
+        ],
+        correctAnswer: 1
+    },
+]
+localStorage.setItem("myQuestions", JSON.stringify)
 var score = 0;
 
+var secondsLeft = 90;
 
 
 
-start.addEventListener("click", startQuiz) 
-
-    
 function startQuiz() {
     start.style.display = "none";
     cardElement.style.display = "block";
     initiateQuestions()
- }
-
-function initiateQuestions(questions) {
-    const myQuestions = [
-        { question:"What is the correct algorithm for a for-loop?",
-    
-        answers: { 
-    
-            a: 'for ( var i = 0; i > array.length; i++)',
-            b: 'for ( var i=0; i < array.length; i++)',
-            c: 'for ( var i = 0; i < array.length; i++)',
-            d: 'for ( var i = 0, i < array.length; i++)'
-         },
-    
-        correctAnswer: 'c'
-        },
-    
-        { question: "Which of the following is NOT used for DECLARATIONS?",
-    
-            answers: {
-               
-                a: 'var',
-                b: 'use',
-                c: 'const',
-                d: 'let'
-            },
-        
-            correctAnswer: 'b'
-            },
-        { question: "JavaScript is a [blank] typed and [blank] language.",
-    
-            answers: {
-               
-                a: 'loosely, dynamic',
-                b: 'strongly, dynamic',
-                c: 'loosely, static',
-                d: 'strongly, static'
-            },
-            
-            correctAnswer: 'a'
-            },
-    
-            { question: "[blank] represents a logical entity and can have a value of either true or false.",
-    
-            answers: {
-               
-                a: 'array',
-                b: 'string',
-                c: 'null',
-                d: 'boolean'
-            },
-            
-            correctAnswer: 'd'
-            },
-    
-            { question: "A variable declared in [blank] is available to all functions?",
-    
-            answers: {
-               
-                a: 'local scope',
-                b: 'internal scope',
-                c: 'global scope',
-                d: 'wide scope'
-            },
-            
-            correctAnswer: 'c'
-            },
-            { question: "[blank] are reusable blacks of code that perform a specific task?",
-    
-            answers: {
-               
-                a: 'objects',
-                b: 'declarations',
-                c: 'perameters',
-                d: 'functions'
-            },
-            
-            correctAnswer: 'd'
-            },
-            { question: "Which of the following stops the functions from being executed?",
-    
-            answers: {
-               
-                a: 'return',
-                b: 'stop',
-                c: 'pause',
-                d: 'loop'
-            },
-            
-            correctAnswer: 'a'
-            },
-            { question: "which represents equal value and equal type?",
-    
-            answers: {
-               
-                a: '==',
-                b: '===',
-                c: '=',
-                d: '/='
-            },
-            
-            correctAnswer: 'b'
-            },
-            { question: "[Blank] are a collection of [blank]. [Blank] are made up of key-value pairs?",
-    
-            answers: {
-               
-                a: 'objects, properties, properties',
-                b: 'properties, objects, objects',
-                c: 'objects, properties, objects',
-                d: 'properties, objects, properties'
-            },
-            
-            correctAnswer: 'a'
-            },
-            { question: "True or False: Functions can not be called again to execute the block of code.",
-    
-            answers: {
-               
-                a: 'true',
-                b: 'false'
-        
-            },
-            
-            correctAnswer: 'b'
-            },
-    ]
-    localStorage.setItem("myQuestions", JSON.stringify)
-    questionContainer.innerText = myQuestions.question
 }
 
+function initiateQuestions() {
+    // TODO: loop through all of the current question's possible answers
+    var currentQuestion = myQuestions[currentIndex];
+    for (var i = 0; i < currentQuestion.answers.length; i++) {
+        var answerText = currentQuestion.answers[i];
+        console.log(answerText);
+    }
+}
+   // console.log(myQuestions[i])
+  //  localStorage.setItem("myQuestions", JSON.stringify)
+  //  questionContainer.innerText = myQuestions.question
+
+  start.addEventListener("click", startQuiz);
